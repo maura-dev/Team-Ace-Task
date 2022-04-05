77b2e644bf6ad7104743968ae9d9d51aa2beef35
zendus <uzendujmike@gmail.com>
2022-04-05 08:13:27 +0100

    feat: implemented purchase feature

--- contracts/NestcoinToken.sol
+++ contracts/NestcoinToken.sol
@@ -5,8 +5,18 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NestcoinToken is ERC20 {
+  address private _owner;
+
+  event Purchase (address indexed from,  uint256 value, string item);
 
   constructor() ERC20("Nestcoin", "NXT") {
-        _mint(msg.sender, 2000 * 10 **18);
+        _mint(msg.sender, 2000 * 10 ** 18);
+        _owner = msg.sender;
     }
+
+  function purchase(uint256 amount, string memory item) public returns (bool) {
+    transfer(_owner, amount);
+    emit Purchase(msg.sender, amount, item);
+    return true;
+  }
 }
